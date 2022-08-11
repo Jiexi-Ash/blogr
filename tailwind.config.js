d876e0473d9bf0b58ad9d3480897296db77ce4a7
Jiexi-Ash <lance.jiexi@gmail.com>
2022-08-11 14:06:36 +0200

    added custom tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "440px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      maxWidth: {
+        "8xl": "1440px",
+      },
+      colors: {
+        grayishBlue: "hsl(240, 2%, 79%)",
+        VeryDarkGrayishBlue: "hsl(207, 13%, 34%)",
+        darkBlackBlue: "hsl(240, 10%, 16%)",
+        veryLightRed: "hsl(13, 100%, 72%)",
+        lightRed: "hsl(353, 100%, 62%)",
+        bgBodyVeryDarkGrayishBlue: "hsl(237, 17%,21%)",
+        desaturatedBlue: "hsl(237, 23%, 32%)",
+      },
+      fontFamily: {
+        overPass: ["Overpass", "sans-serif"],
+        ubu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
